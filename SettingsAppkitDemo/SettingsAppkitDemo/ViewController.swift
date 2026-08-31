e213dbe2d4dc4c6d8bab71e2a030ba58ad7d74d2
Izumiiii <depressing113@foxmail.com>
2026-08-31 16:55:25 +0800

    SettingsUI demo

---
+++ SettingsAppkitDemo/SettingsAppkitDemo/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  SettingsAppkitDemo
+//
+//  Created by wangjie on 2026/8/31.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
